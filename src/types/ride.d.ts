e5364152187d3b671c3afae447f233b53da1676c
Jose <pedrojcunha7@hotmail.com>
2025-03-24 13:09:47 +0100

    chore: setup json server and created ride interface

---
+++ src/types/ride.d.ts
@@ -0,0 +1,13 @@
+export interface Ride {
+    id: number;
+    title: string;
+    difficulty: number;
+    description: string;
+    location: {
+        city: string;
+        npa: number;
+        addressName: string;
+        addressNumber: number;
+    };
+    images: string[];
+}
