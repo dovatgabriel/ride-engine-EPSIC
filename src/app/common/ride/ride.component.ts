7b3f52acf3ba0e1d5c28d3ef2799797ad86c544f
Adaptati0n <adaptati0nbtc@outlook.com>
2025-04-07 13:16:48 +0200

    feat: redirection into details page

--- src/app/common/ride/ride.component.ts
+++ src/app/common/ride/ride.component.ts
@@ -1,13 +1,23 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Ride } from '../../../types/ride';
 import { DifficultyColorPipe } from '../../pipes/difficulty-color.pipe';
 
 @Component({
   selector: 'app-ride',
+  standalone: true,
   imports: [DifficultyColorPipe],
   templateUrl: './ride.component.html',
   styleUrl: './ride.component.scss',
 })
 export class RideComponent {
   @Input() ride: Ride | undefined;
+
+  constructor(private router: Router) {}
+
+  goToDetail(): void {
+    if (this.ride) {
+      this.router.navigate(['/rides', this.ride.id]);
+    }
+  }
 }
