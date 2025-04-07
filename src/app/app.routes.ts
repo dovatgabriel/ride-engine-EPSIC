19fabb57c07b2d279573a36c749726175cc3f623
Adaptati0n <adaptati0nbtc@outlook.com>
2025-04-07 13:16:48 +0200

    feat: redirection into details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { RideComponent } from './common/ride/ride.component';
 
 export const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'rides/:id',
+    component: RideComponent, 
   },
   {
     path: 'home',
