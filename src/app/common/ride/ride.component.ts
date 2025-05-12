706e4f26298380ebf1a28c81e5b551eab5904655
Adaptati0n <adaptati0nbtc@outlook.com>
2025-05-12 10:23:56 +0200

    fix: remove commentary

--- src/app/common/ride/ride.component.ts
+++ src/app/common/ride/ride.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common'; // ✅ Ajouté ici
+import { CommonModule } from '@angular/common'; 
 import { Router } from '@angular/router';
 import { Ride } from '../../../types/ride';
 
